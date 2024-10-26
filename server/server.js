3ab8a204b386bef836087f07c2bcc430968cc30b
Ayoub Bigharassine <ayoub.bigharassine@gmail.com>
2024-10-26 17:29:46 +0100

    organize file structure

--- server/server.js
+++ server/server.js
@@ -1,24 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 
 app.use(express.json());
 app.use(cors());
 
+// Routes
+app.get("/", (req, res) => res.send("Hello World!"));
+
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
+mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.error(err));
-
-// Define Routes
-app.use('/api', require('./routes'));
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
 
 app.listen(PORT, () => console.log(`Server runnin on port ${PORT}`));
