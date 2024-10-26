5ba920ff469c9bf3db00652e1359cc2c829cf14a
imanewkm <imanewkm02@gmail.com>
2024-10-26 23:06:55 +0100

    Live Chat option

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import React from "react";
+import Chat from "./Chat/Chat";
 
 function App() {
+  // example contactId
+  const contactId = "123";
+
   return (
-    <div>
-      App
+    <div className="App">
+      <h1>Welcome to SyncChat</h1>
+      <Chat contactId={contactId} />
     </div>
   );
 }
